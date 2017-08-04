f42de96ac92a47e0d67536f2bf93ef39915b8ec5
OliverPark <chanho16.park@gmail.com>
2017-08-04 13:38:31 +0000

    feat: Implemente Model1 completely

--- sample_models.py
+++ sample_models.py
@@ -27,13 +27,10 @@ def rnn_model(input_dim, units, activation, output_dim=29):
     # Add recurrent layer
     simp_rnn = GRU(units, activation=activation,
         return_sequences=True, implementation=2, name='rnn')(input_data)
-    # TODO: Add batch normalization 
-    bn_rnn = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, 
-        beta_initializer='zeros', gamma_initializer='ones', 
-        moving_mean_initializer='zeros', moving_variance_initializer='ones', 
-        beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
-    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim), input_shape=input_dim)
+    # DONE: Add batch normalization 
+    bn_rnn = BatchNormalization()(simp_rnn)
+    # DONE: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -61,12 +58,9 @@ def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
     simp_rnn = SimpleRNN(units, activation='relu',
         return_sequences=True, implementation=2, name='rnn')(bn_cnn)
     # TODO: Add batch normalization
-    bn_rnn = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True,
-        beta_initializer='zeros', gamma_initializer='ones',
-        moving_mean_initializer='zeros', moving_variance_initializer='ones',
-        beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
+    bn_rnn = BatchNormalization()(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim), input_shape=input_dim)
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -106,12 +100,9 @@ def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
     # TODO: Add recurrent layers, each with batch normalization
     simp_rnn = GRU(units, activation=activation,
         return_sequences=True, implementation=2, name='rnn')(input_data)
-    bn_rnn = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True,
-        beta_initializer='zeros', gamma_initializer='ones',
-        moving_mean_initializer='zeros', moving_variance_initializer='ones',
-        beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
+    bn_rnn = BatchNormalization()(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim), input_shape=input_dim)
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -126,9 +117,9 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Add bidirectional recurrent layer
-    bidir_rnn = ...
+    bidir_rnn = Bidirectional(LSTM(output_dim, return_sequences=True))(input_data)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bidir_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
